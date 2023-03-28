283cdf2b596158d317178125f91ba9d76934626f
mzamo <mzamo@MZAMOS_LAPTOP>
2023-03-28 22:18:22 +0200

    Add project files.

---
+++ Models/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Architecture.Models
+{
+    public interface ICourseRepository
+    {
+        // Course
+        Task<Course[]> GetAllCourseAsync();
+
+        Task<Course> GetCourseAsync(int id);
+
+        Task<Course> AddCourseAsync(Course NewCourse);
+        Task<Course> DeleteCourseAsync(int id);
+        Task<Course> UpdateCourseAsync(int id, Course updateCourseRequest);
+    }
+}
